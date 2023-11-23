bafd338aa61031d9d94935695e2537a778ed5450
duckypixel <98136801+duckypixel@users.noreply.github.com>
2023-11-23 22:04:19 +0700

    start primitive types

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(amt: i32) -> i32 {
+    let applePrice: i32 = 2;
+    if amt > 40 {
+        amt
+    } else {
+        applePrice * amt
+    }
+}
 
 // Don't modify this function!
 #[test]
